46fe921b0c9a90e570b8f96759291da076c1bc1b
denis.kalenga <engineereldorado@gmail.com>
2018-07-07 17:11:33 +0300

    fixed the JavaFx issue

--- src/main/java/com/zara/Zara/ZaraApplication.java
+++ src/main/java/com/zara/Zara/ZaraApplication.java
@@ -2,11 +2,14 @@ package com.zara.Zara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ZaraApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZaraApplication.class, args);
