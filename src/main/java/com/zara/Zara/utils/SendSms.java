ccf4a9649d0aaf613c1bcc1ebc27377a6296acaf
denis.kalenga <engineereldorado@gmail.com>
2018-07-26 15:03:08 +0300

    ONLY SEND SMS IF IT HAS BEEN ENABLED

--- src/main/java/com/zara/Zara/utils/SendSms.java
+++ src/main/java/com/zara/Zara/utils/SendSms.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 
+import com.zara.Zara.entities.Setting;
+import com.zara.Zara.services.ISettingService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -16,31 +18,39 @@ public class SendSms {
 
    static Logger LOGGER = LogManager.getLogger(SendSms.class);
 
-    public static String send( String phonenumber,String msg){
-        try{
+    public static String send(String phonenumber, String msg, ISettingService settingService){
+        Setting setting = settingService.getSettingById(1L);
+        if(setting.isSmsEnabled()){
+            LOGGER.info("......... SENDING..........");
             try{
-                URL smslink = new
-                        URL(MOVE_SMS_URL+"compose?"+"username="+MOVE_SMS_USERNAME+"&api_key="+MOVE_SMS_API_KEY+"&sender="+MOVE_SMS_SENDER_ID+"&to="+phonenumber+"&message="+java.net.URLEncoder.encode(msg, "UTF-8")+"&msgtype=5&dlr=0");
-                BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        smslink.openStream()));
-                String inputLine;
-                while ((inputLine = in.readLine()) != null)
-                    System.out.println(inputLine);
+                try{
+                    URL smslink = new
+                            URL(MOVE_SMS_URL+"compose?"+"username="+MOVE_SMS_USERNAME+"&api_key="+MOVE_SMS_API_KEY+"&sender="+MOVE_SMS_SENDER_ID+"&to="+phonenumber+"&message="+java.net.URLEncoder.encode(msg, "UTF-8")+"&msgtype=5&dlr=0");
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(
+                                    smslink.openStream()));
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null)
+                        System.out.println(inputLine);
 
-                System.out.println("Sms send successfully!");
-                LOGGER.info("Sms send successfully!");
-                //System.out.println("Api: " +smslink);
+                    //System.out.println("Sms send successfully!");
+                    LOGGER.info("Sms send successfully!");
+                    //System.out.println("Api: " +smslink);
 
-                in.close();
+                    in.close();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    LOGGER.info("Sms Connection not Found");
+                    throw new BadCredentialsException("Sms Connection not Found");
+                }
             }catch (Exception e) {
                 e.printStackTrace();
-                LOGGER.info("Sms Connection not Found");
-                throw new BadCredentialsException("Sms Connection not Found");
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
 
+            }
+
+        }
+        else{
+              LOGGER.info("......... SMS IS DESABLED..........");
         }
         return null;
 
