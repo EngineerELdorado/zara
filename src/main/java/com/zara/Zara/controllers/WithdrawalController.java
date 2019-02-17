1c7af1144a1d8a07b9cb791adf87d6f33df94ce1
Denis Kalenga <engineereldorado@gmail.com>
2019-02-17 16:00:51 +0300

    agent creation implementation

---
+++ src/main/java/com/zara/Zara/controllers/WithdrawalController.java
@@ -0,0 +1,138 @@
+package com.zara.Zara.controllers;
+
+import com.zara.Zara.constants.ApiResponse;
+import com.zara.Zara.entities.Agent;
+import com.zara.Zara.entities.Customer;
+import com.zara.Zara.entities.PesapayTransaction;
+import com.zara.Zara.models.Sms;
+import com.zara.Zara.models.TransactionRequestBody;
+import com.zara.Zara.services.IAgentService;
+import com.zara.Zara.services.ICustomerService;
+import com.zara.Zara.services.ITransactionService;
+import com.zara.Zara.services.utils.SmsService;
+import com.zara.Zara.utils.BusinessNumbersGenerator;
+import com.zara.Zara.utils.GenerateRandomStuff;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static com.zara.Zara.constants.ConstantVariables.TRANSACTION_CUSTOMER_RANSFER;
+import static com.zara.Zara.constants.ConstantVariables.TRANSACTION_DEPOSIT;
+import static com.zara.Zara.constants.ConstantVariables.TRANSACTION_WITHDRAWAL;
+
+@RestController
+@RequestMapping("/withdrawals")
+public class WithdrawalController {
+
+
+    ApiResponse apiResponse = new ApiResponse();
+    Sms sms = new Sms();
+    @Autowired
+    ICustomerService customerService;
+    @Autowired
+    ITransactionService transactionService;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    IAgentService agentService;
+    Logger LOGGER = LogManager.getLogger(CustomerTransferController.class);
+
+    @PostMapping("/post")
+    public ResponseEntity<?>post(@RequestBody TransactionRequestBody request) throws UnsupportedEncodingException {
+
+        Agent agent = agentService.findByAgentNumber(request.getAgentNumber());
+        Customer customer = customerService.findByPhoneNumber(request.getSender());
+        if (agent==null){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Ce numero agent n'existe pas");
+            LOGGER.info("AGENT NUMBER NOT FOUND FOR "+ request.getAgentNumber());
+        }else if (!agent.getStatus().equals("ACTIVE")){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Cet compte agent est bloquE");
+            LOGGER.info("ACCOUNT NOT ACTIVE FOR AGENT "+agent.getAgentNumber());
+        }else if (!agent.isVerified()){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Agent non verifiE");
+            LOGGER.info("ACCOUNT NOT VERIFIED FOR AGENT "+agent.getAgentNumber());
+        }
+        else if (customer==null){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Ce numero de client n'existe pas");
+            LOGGER.info("CUSTOMER ACCOUNT NOT FOUND");
+        }
+        else if (!customer.getStatus().equals("ACTIVE")){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Le compte du client n'est pas actif\n "+customer.getStatusDescription());
+            LOGGER.info("CUSTOMER ACCOUNT NOT ACTIVE");
+        }else if (!customer.isVerified()){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Le compte du client n'est pas encore verifie\n "+customer.getStatusDescription());
+            LOGGER.info("CUSTOMER ACCOUNT NOT VERIFIED");
+        }
+        else if (customer.getBalance().compareTo(new BigDecimal(request.getAmount()))<0){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Solde Insuffisant "+agent.getBalance()+" USD");
+            LOGGER.info("CUSTOMER BALANCE INSUFFICIENT FOR CUSTOMER "+customer.getFullName());
+        }else if (!bCryptPasswordEncoder.matches(request.getPin(), customer.getPin())){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Pin Incorrect");
+            LOGGER.info("INCORRECT PIN FOR CUSTOMER "+ agent.getAgentNumber());
+        }else{
+            PesapayTransaction transaction = new PesapayTransaction();
+            transaction.setAmount(new BigDecimal(request.getAmount()));
+            transaction.setCreatedOn(new Date());
+            transaction.setStatus("00");
+            transaction.setDescription("Withdrawal successful");
+            transaction.setTransactionNumber(BusinessNumbersGenerator.generateTransationNumber(transactionService));
+            transaction.setCreatedCustomer(customer);
+            transaction.setReceivedByAgent(agent);
+            transaction.setTransactionType(TRANSACTION_WITHDRAWAL);
+
+            PesapayTransaction createdTransaction = transactionService.addTransaction(transaction);
+            if (createdTransaction==null){
+                apiResponse.setResponseCode("01");
+                apiResponse.setResponseMessage("ECHEC");
+                LOGGER.info("TRANSACTION FAILED TO PERSIST TO DATABASE");
+            }else {
+
+                customer.setBalance(customer.getBalance().subtract(new BigDecimal(request.getAmount())));
+                Customer updatedCustomer = customerService.save(customer);
+
+                Sms sms1 = new Sms();
+                sms1.setTo(customer.getPhoneNumber());
+                sms1.setMessage(customer.getFullName()+ " vous venez de retirer de votre compte "+request.getAmount()+"USD au numero agent "+agent.getAgentNumber()+" "+agent.getFullName()+" via PesaPay. "+
+                        " type de transaction RETRAIT DIRECT. votre solde actuel est "+updatedCustomer.getBalance()+" USD");
+                SmsService.sendSms(sms1);
+
+                agent.setBalance(agent.getBalance().add(new BigDecimal(request.getAmount())));
+                Agent updatedAgent = agentService.save(agent);
+                Sms sms2 = new Sms();
+                sms2.setTo(agent.getPhoneNumber());
+                sms2.setMessage(agent.getFullName()+ " vous venez de recevoir "+request.getAmount()+"USD venant de "+customer.getFullName()+" via PesaPay. "+
+                        " type de transaction RETRAIT DIRECT. votre solde actuel est "+updatedAgent.getBalance()+" USD");
+                SmsService.sendSms(sms2);
+
+
+
+                apiResponse.setResponseCode("00");
+                apiResponse.setResponseMessage("TRANSACTION REUSSIE");
+                LOGGER.info("DEPOSIT TRANSACTION SUCCESSFUL "+transaction.getTransactionNumber());
+
+            }
+        }
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+}
