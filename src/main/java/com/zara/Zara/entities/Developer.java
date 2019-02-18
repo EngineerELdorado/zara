0b0d05d47cedbb48febd10b5ed62b13c499b8c94
Denis Kalenga <engineereldorado@gmail.com>
2019-02-18 15:46:26 +0300

    Developer account implementation

--- src/main/java/com/zara/Zara/entities/Developer.java
+++ src/main/java/com/zara/Zara/entities/Developer.java
@@ -3,14 +3,15 @@ package com.zara.Zara.entities;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Data
 @Entity(name = "developers")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Developer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String email;
     private String name;
