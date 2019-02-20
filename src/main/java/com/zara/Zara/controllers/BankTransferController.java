91beebf0223344f7d79817cfb4703c05b414efba
Denis Kalenga <engineereldorado@gmail.com>
2019-02-20 15:06:46 +0300

    bank transfers implementation

--- src/main/java/com/zara/Zara/controllers/BankTransferController.java
+++ src/main/java/com/zara/Zara/controllers/BankTransferController.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class BankTransferController {
     SafePayService safePayService;
     Logger LOGGER = LogManager.getLogger(BankTransferController.class);
     ApiResponse apiResponse = new ApiResponse();
-    @RequestMapping("/post")
+    @PostMapping("/post")
     public ResponseEntity<?>post(@RequestBody TransactionRequestBody requestBody){
 
         SafePayRequest request = new SafePayRequest();
