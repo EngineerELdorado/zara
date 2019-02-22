373fa778be2411f626ffb2950c754a593c610b48
Denis Kalenga <engineereldorado@gmail.com>
2019-02-22 18:37:51 +0300

    bulk payments implementation

--- src/main/java/com/zara/Zara/controllers/BulkPaymentController.java
+++ src/main/java/com/zara/Zara/controllers/BulkPaymentController.java
@@ -10,6 +10,8 @@ import com.zara.Zara.models.TransactionRequestBody;
 import com.zara.Zara.services.*;
 import com.zara.Zara.services.utils.SmsService;
 import com.zara.Zara.utils.BusinessNumbersGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,11 +45,14 @@ public class BulkPaymentController {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
     ApiResponse apiResponse = new ApiResponse();
-
+    boolean insufficientBalanceMessageAlreadySent=false;
+    Logger LOGGER = LogManager.getLogger(BankTransferController.class);
     @PostMapping("/post}")
-    public ResponseEntity<?>post(@RequestBody TransactionRequestBody requestBody){
+    public ResponseEntity<?>post(@RequestBody TransactionRequestBody requestBody) throws UnsupportedEncodingException {
         Business business = businessService.findByBusinessNumber(requestBody.getSender());
+        LOGGER.info("BUSINESS "+business.toString());
         Collection<BulkBeneficiary>bulkBeneficiaries = bulkBeneficiaryService.findByCaterory(Long.parseLong(requestBody.getBulkCategoryId()));
+        LOGGER.info("TOTAL OF BENEFICIARIES =>"+bulkBeneficiaries.size());
         int successCount =0;
         Business updatedBusiness = null;
         int failureCount = 0;
@@ -56,24 +61,32 @@ public class BulkPaymentController {
             if (business.getStatus().equals("ACTIVE")){
                 if (bCryptPasswordEncoder.matches(requestBody.getPin(), business.getPin())){
                     for (BulkBeneficiary beneficiary: bulkBeneficiaries){
+
+                        LOGGER.info("PROCESSING PAYMENT FOR "+beneficiary.getName());
                         ExecutorService executorService = Executors.newFixedThreadPool(8);
                         Customer customer = customerService.findByPhoneNumber(beneficiary.getPhoneNumber());
                         PesapayTransaction transaction = new PesapayTransaction();
                         transaction.setCreatedOn(new Date());
                         transaction.setCreatedByBusiness(business);
                         if (business.getStatus().equals("ACTIVE")){
+                            LOGGER.info("ACCOUNT IS ACTIVE FOR "+business.getBusinessName());
                             if (customer!=null){
-                                if (customer.isVerified()){
+                                LOGGER.info("ACCOUNT EXISTS FOR "+customer.getFullName());
+                                if (!customer.isVerified()){
                                     transaction.setStatus("01");
                                     transaction.setTransactionNumber(BusinessNumbersGenerator.generateTransationNumber(transactionService));
                                     transaction.setDescription("Transaction echoue. compte non verifie pour le numero "+beneficiary.getPhoneNumber());
                                     failureCount++;
+                                    LOGGER.info("TRANSACTION FAILED ACCOUNT NOT VERIFIED "+customer.getFullName());
+                                    updatedBusiness=business;
                                     transactionService.addTransaction(transaction);
                                 }else if (!customer.getStatus().equals("ACTIVE")){
                                     transaction.setStatus("01");
                                     transaction.setTransactionNumber(BusinessNumbersGenerator.generateTransationNumber(transactionService));
                                     transaction.setDescription("Transaction echoue. compte NON ACTIF pour le numero "+beneficiary.getPhoneNumber());
                                     failureCount++;
+                                    LOGGER.info("TRANSACTION FAILED ACCOUNT NOT ACTIVE "+customer.getFullName());
+                                    updatedBusiness=business;
                                     transactionService.addTransaction(transaction);
                                 }
 
@@ -81,7 +94,12 @@ public class BulkPaymentController {
                                     transaction.setStatus("01");
                                     transaction.setTransactionNumber(BusinessNumbersGenerator.generateTransationNumber(transactionService));
                                     transaction.setDescription("Transaction echoue. SOLDE DU BUSINESS EST EPUISE ");
+                                    LOGGER.info("TRANSACTION FAILED BUSINESS BALANCE INSUFFICIENT ");
+                                    if (!insufficientBalanceMessageAlreadySent){
+                                        sendBalanceExaustedOnlyOnce(business.getPhoneNumber(), business.getBusinessName());
+                                    }
                                     failureCount++;
+                                    updatedBusiness=business;
                                     transactionService.addTransaction(transaction);
                                 }else {
                                     transaction.setStatus("00");
@@ -93,6 +111,7 @@ public class BulkPaymentController {
                                     business.setBalance(business.getBalance().subtract(beneficiary.getAmount()));
                                     Customer updatedCustomer = customerService.save(customer);
                                     updatedBusiness = businessService.save(business);
+                                    LOGGER.info("TRANSACTION SUCCESSFUL "+customer.getFullName());
                                     Sms sms = new Sms();
                                     sms.setTo(customer.getPhoneNumber());
                                     sms.setMessage(customer.getFullName()+" vous avez recu "+beneficiary.getAmount()+" USD venant de "+business.getBusinessName()+" pour "+requestBody.getDescription()+" votre solde actuel est de "+updatedCustomer.getBalance()+" USD");
@@ -106,14 +125,16 @@ public class BulkPaymentController {
                                 transaction.setTransactionNumber(BusinessNumbersGenerator.generateTransationNumber(transactionService));
                                 transaction.setStatus("01");
                                 transaction.setDescription("Transaction echoue. compte introuvable pour le numero "+beneficiary.getPhoneNumber());
-
+                                updatedBusiness=business;
+                                LOGGER.info("TRANSACTION FAILED ACCOUNT NOT FOUND "+beneficiary.getName());
                             }
                         }
                     }
                     Sms sms = new Sms();
                     sms.setTo(business.getPhoneNumber());
                     sms.setMessage(business.getBusinessName()+" vous venez d'effectuer "+bulkBeneficiaries.size()+" payments dont "+successCount+" reussis et "+failureCount+" echoues. votre solde actuel est de "+updatedBusiness.getBalance()+" USD");
-                }else{
+                    SmsService.sendSms(sms);
+                 }else{
                     apiResponse.setResponseCode("01");
                     apiResponse.setResponseMessage("Pin Incorrect");
                 }
@@ -129,5 +150,13 @@ public class BulkPaymentController {
 
 
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    public void sendBalanceExaustedOnlyOnce(String to, String name) throws UnsupportedEncodingException {
+        Sms sms = new Sms();
+        sms.setTo(to);
+        sms.setMessage(name+ " Votre solde est epuise. les payments vont echouer");
+        SmsService.sendSms(sms);
+        insufficientBalanceMessageAlreadySent=true;
     }
 }
