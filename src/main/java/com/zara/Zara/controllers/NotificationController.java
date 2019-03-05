9e481b9a0665a245d0e6c1ed7f575e56901eda3f
Denis Eldorado <engineereldorado@gmail.com>
2019-03-05 15:40:12 +0100

    business portal

--- src/main/java/com/zara/Zara/controllers/NotificationController.java
+++ src/main/java/com/zara/Zara/controllers/NotificationController.java
@@ -11,10 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notifications")
@@ -27,7 +24,7 @@ public class NotificationController {
     IBusinessService businessService;
     ApiResponse apiResponse = new ApiResponse();
     @GetMapping("/findByBusiness/{businessNumber}")
-    public ResponseEntity<?> findByBusiness(String businessNumber, int page, int size){
+    public ResponseEntity<?> findByBusiness(@PathVariable String businessNumber, int page, int size){
         Sort sort = new Sort(new Sort.Order(Sort.Direction.DESC,"id"));
         Pageable pageable = new PageRequest(page,size,sort);
         Business business = businessService.findByBusinessNumber(businessNumber);
