661044dfcd7c193cff1df72b402015282f3bad3f
Denis Eldorado <engineereldorado@gmail.com>
2019-03-07 15:15:51 +0100

    business portal

--- src/main/java/com/zara/Zara/controllers/CreditCardTransactionController.java
+++ src/main/java/com/zara/Zara/controllers/CreditCardTransactionController.java
@@ -2,7 +2,9 @@ package com.zara.Zara.controllers;
 
 import com.stripe.exception.*;
 import com.stripe.model.Charge;
+import com.zara.Zara.entities.Business;
 import com.zara.Zara.entities.PesapayTransaction;
+import com.zara.Zara.services.IBusinessService;
 import com.zara.Zara.services.banking.StripeService;
 import com.zara.Zara.utils.BusinessNumbersGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +47,8 @@ public class CreditCardTransactionController {
     @Autowired
     StripeService stripeService;
     Charge charge;
+    @Autowired
+    IBusinessService businessService;
     @PostMapping("/post")
     public ResponseEntity<?> post(@RequestBody TransactionRequestBody request) throws UnsupportedEncodingException, CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
         Customer senderCustomer = customerService.findByPhoneNumber(request.getReceiver());
@@ -117,4 +121,82 @@ public class CreditCardTransactionController {
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
 
 }
+
+
+
+
+    @PostMapping("/post/business")
+    public ResponseEntity<?> postForBusiness(@RequestBody TransactionRequestBody request) throws UnsupportedEncodingException, CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
+        Business business = businessService.findByBusinessNumber(request.getReceiver());
+
+        if (business==null){
+            apiResponse.setResponseCode("01");
+            apiResponse.setResponseMessage("Votre compte n'existe pas");
+            LOGGER.info("SENDER ACCOUNT NOT FOUND FOR "+request.getReceiver());
+        }
+
+        else{
+            try {
+                ChargeRequest chargeRequest = new ChargeRequest();
+                chargeRequest.setAmount(Integer.valueOf(request.getAmount()));
+                chargeRequest.setCurrency(ChargeRequest.Currency.USD);
+                chargeRequest.setDescription(" Transaction request by PesaPay User "+business.getBusinessName());
+                chargeRequest.setStripeToken(request.getStripeToken());
+
+                charge = stripeService.charge(chargeRequest);
+                if (charge.getStatus().equals("succeeded")){
+                    apiResponse.setResponseCode("00");
+                    apiResponse.setResponseMessage("Transaction Reussie");
+
+                    PesapayTransaction transaction = new PesapayTransaction();
+                    transaction.setAmount(new BigDecimal(request.getAmount()));
+                    transaction.setCreatedOn(new Date());
+                    transaction.setStatus("00");
+                    transaction.setDescription("Deposit(card) successful");
+                    transaction.setTransactionNumber(BusinessNumbersGenerator.generateTransationNumber(transactionService));
+                    transaction.setReceivedByBusiness(business);
+                    transaction.setTransactionType(TRANSACTION_CREDIT_CARD_DEPOSIT);
+
+                    PesapayTransaction createdTransaction = transactionService.addTransaction(transaction);
+                    if (createdTransaction==null){
+                        apiResponse.setResponseCode("01");
+                        apiResponse.setResponseMessage("ECHEC");
+                        LOGGER.info("TRANSACTION FAILED TO PERSIST TO DATABASE");
+                    }else {
+
+
+                        business.setBalance(business.getBalance().add(new BigDecimal(request.getAmount())));
+                        Business updatedBusiness = businessService.save(business);
+
+                        Sms sms2 = new Sms();
+                        sms2.setTo(business.getPhoneNumber());
+                        sms2.setMessage(business.getBusinessName()+ " Vous venez de recevoir "+request.getAmount()+"USD venant de la carte bancaire"+
+                                " du numero "+request.getSender()+" "+" via PesaPay. "+
+                                " type de transaction DEPOT VIA CARTE BANCAIRE. votre solde actuel est "+updatedBusiness.getBalance()+" USD. numero de transaction "+transaction.getTransactionNumber());
+                        SmsService.sendSms(sms2);
+
+                        apiResponse.setResponseCode("00");
+                        apiResponse.setResponseMessage("TRANSACTION REUSSIE");
+                        LOGGER.info("DEPOSIT TRANSACTION SUCCESSFUL "+transaction.getTransactionNumber());
+                    }
+
+                }else{
+                    apiResponse.setResponseCode("01");
+                    apiResponse.setResponseMessage(charge.getFailureMessage());
+                    LOGGER.info("STRIPE_FAILURE_MESSAGE "+charge.getFailureMessage());
+                }
+
+            }catch (Exception e){
+                apiResponse.setResponseCode("01");
+                apiResponse.setResponseMessage(e.getLocalizedMessage());
+                LOGGER.info("STRIPE_FAILURE_MESSAGE "+e.getLocalizedMessage());
+                e.printStackTrace();
+            }
+
+        }
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+
+    }
+
+
 }
