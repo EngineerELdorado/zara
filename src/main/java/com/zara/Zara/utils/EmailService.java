e0e1dc8fede57192c9dae651c31e4bbac81f50df
Denis Eldorado <engineereldorado@gmail.com>
2019-03-24 07:34:05 +0100

    business app integration

--- src/main/java/com/zara/Zara/utils/EmailService.java
+++ src/main/java/com/zara/Zara/utils/EmailService.java
@@ -25,7 +25,7 @@ public class EmailService {
             }
         });
         Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress("PesaPay", true));
+        msg.setFrom(new InternetAddress("pesapaydev@gmail.com", "PesaPay Ltd"));
 
         msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
         msg.setSubject("EMAIL VERIFICATION");
