33293faef96f94fa8735cc92bbff6c40c9666187
denis <nationaldatasystem@gmail.com>
2019-04-27 17:42:50 +0200

    implementing charges on transactions

--- src/main/java/com/zara/Zara/listeners/AppEventListener.java
+++ src/main/java/com/zara/Zara/listeners/AppEventListener.java
@@ -1,14 +1,18 @@
 package com.zara.Zara.listeners;
 
 
+import com.zara.Zara.entities.PesapayTransaction;
 import com.zara.Zara.entities.Role;
 import com.zara.Zara.services.IRoleService;
+import com.zara.Zara.services.ITransactionService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import static com.zara.Zara.constants.ConstantVariables.*;
@@ -20,47 +24,34 @@ public class AppEventListener implements CommandLineRunner {
     @Autowired
     IRoleService roleService;
 
+    @Autowired
+    ITransactionService transactionService;
+
     Logger LOGGER = LogManager.getLogger(AppEventListener.class);
 
     @Override
     public void run(String... args) {
-        LOGGER.info("............APP HAS STARTED...........");
+        LOGGER.info("............PESAPAY BACKEND HAS STARTED...........");
 
-       Collection<Role> roles= roleService.all();
-       if(roles.size()==0){
-           Role roleSuperAdmin = new Role();
-           roleSuperAdmin.setName(ROLE_SUPERADMIN);
-           roleService.add(roleSuperAdmin);
-           LOGGER.info("............ADDED SUPER ADMIN ROLE...........");
-           Role roleAdmin = new Role();
-           roleAdmin.setName(ROLE_ADMIN);
-           roleService.add(roleAdmin);
-           LOGGER.info("............ADDED ADMIN ROLE...........");
-           Role roleStaff= new Role();
-           roleStaff.setName(ROLE_STAFF);
-           roleService.add(roleStaff);
-           LOGGER.info("............ADDED STAFF ROLE...........");
+        Collection<PesapayTransaction> transactions = transactionService.getAll();
+        int count =0;
+        for(PesapayTransaction transaction: transactions){
 
-           Role roleUser= new Role();
-           roleUser.setName(ROLE_USER);
-           roleService.add(roleUser);
-           LOGGER.info("............ADDED USER ROLE...........");
+            BigDecimal amount = transaction.getAmount();
+            BigDecimal charges = amount.multiply(new BigDecimal("5")).divide(
+                    new BigDecimal("100")
+            );
+            BigDecimal sent = amount.subtract(charges);
+            transaction.setAmount(sent);
+            transaction.setCharges(charges);
+            transactionService.addTransaction(transaction);
+            LOGGER.info("UPDATED TRANSACTION "+ count +"\n amount :"+amount
+            +"\n charges: "+charges+" \n sent: "+sent);
+        }
 
-           Role roleAgent= new Role();
-           roleAgent.setName(ROLE_AGENT);
-           roleService.add(roleAgent);
-           LOGGER.info("............ADDED AGENT ROLE...........");
-
-           Role roleMassPayer= new Role();
-           roleAgent.setName(ROLE_MASS_PAYER);
-           roleService.add(roleMassPayer);
-           LOGGER.info("............ADDED USER MASS PAYER ROLE...........");
-
-       }
-       else {
-           LOGGER.info("............ROLES ALREADY EXIST...........");
-       }
     }
+
+
 
 
 }
