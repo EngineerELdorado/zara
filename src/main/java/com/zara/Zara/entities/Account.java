080db3b5b8633a0fa9612f0208285da84e5b8ee2
nationaldata <nationaldatasystem@gmail.com>
2019-06-29 12:48:23 +0200

    withdrawal bug fixed

--- src/main/java/com/zara/Zara/entities/Account.java
+++ src/main/java/com/zara/Zara/entities/Account.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
