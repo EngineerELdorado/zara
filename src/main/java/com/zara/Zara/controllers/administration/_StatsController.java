f204cf344866c50356d85fcaa5c98a436324b2ee
Denis Kalenga <engineereldorado@gmail.com>
2019-10-03 23:56:52 -0700

    admin portal backend setup

--- src/main/java/com/zara/Zara/controllers/administration/_StatsController.java
+++ src/main/java/com/zara/Zara/controllers/administration/_StatsController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping("/admins/stats")
 @CrossOrigin(origins = "*")
@@ -32,14 +34,14 @@ public class _StatsController {
     public ResponseEntity<?>getStat(Long start, Long end){
 
         Stat stat = new Stat();
-        stat.setAgents(agentService.findCount(start,end));
-        stat.setTransactions(transactionService.findCount(start,end));
-        stat.setCustomers(customerService.findCount(start,end));
-        stat.setBusinesses(businessService.findCount(start,end));
-        stat.setTransactions(businessService.findCount(start,end));
-        stat.setAmounts(transactionService.amounts(start,end));
-        stat.setCommissions(transactionService.commissions(start,end));
-        stat.setPending(transactionService.amountsPending(start,end));
+        stat.setAgents(agentService.findCount(start,end)==null?0:agentService.findCount(start,end));
+        stat.setTransactions(transactionService.findCount(start,end)==null?0:transactionService.findCount(start,end));
+        stat.setCustomers(customerService.findCount(start,end)==null?0:customerService.findCount(start,end));
+        stat.setBusinesses(businessService.findCount(start,end)==null?0:businessService.findCount(start,end));
+        stat.setTransactions(transactionService.findCount(start,end)==null?0:businessService.findCount(start,end));
+        stat.setAmounts(transactionService.amounts(start,end)==null?new BigDecimal(0) :transactionService.amounts(start,end));
+        stat.setCommissions(transactionService.commissions(start,end)==null?new BigDecimal(0):transactionService.commissions(start,end));
+        stat.setPending(transactionService.amountsPending(start,end)==null?new BigDecimal(0):transactionService.amountsPending(start,end));
         apiResponse.setData(stat);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
