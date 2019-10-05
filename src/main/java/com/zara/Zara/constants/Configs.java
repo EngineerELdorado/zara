ea53e8e2015e0894b20c58b36a84311eb5d00e8b
Denis Kalenga <engineereldorado@gmail.com>
2019-10-04 19:48:22 -0700

    admin portal backend setup

--- src/main/java/com/zara/Zara/constants/Configs.java
+++ src/main/java/com/zara/Zara/constants/Configs.java
@@ -3,11 +3,11 @@ package com.zara.Zara.constants;
 
 public class Configs {
 
-    public static String PERCENTAGE_ON_C2C ="2.5";
-    public static String PERCENTAGE_ON_C2B ="2.5";
-    public static String PERCENTAGE_ON_B2C ="5";
-    public static String PERCENTAGE_ON_B2B ="5";
-    public static String PERCENTAGE_ON_B2C_BULK="5";
-    public static String PERCENTAGE_ON_B2B_BULK ="5";
-    public static String PERCENTAGE_ON_WITHDRAWAL ="2.5";
+    public static String PERCENTAGE_ON_C2C ="0.0";
+    public static String PERCENTAGE_ON_C2B ="0.0";
+    public static String PERCENTAGE_ON_B2C ="0.0";
+    public static String PERCENTAGE_ON_B2B ="0.0";
+    public static String PERCENTAGE_ON_B2C_BULK="0.0";
+    public static String PERCENTAGE_ON_B2B_BULK ="0.0";
+    public static String PERCENTAGE_ON_WITHDRAWAL ="0.0";
 }
