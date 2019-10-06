ee8feade21bc9ec4b85ebe94c1c60be9c2692aea
Denis Kalenga <engineereldorado@gmail.com>
2019-10-06 00:24:23 -0700

    `admin portal backend setup`

--- src/main/java/com/zara/Zara/entities/CommissionSetting.java
+++ src/main/java/com/zara/Zara/entities/CommissionSetting.java
@@ -3,12 +3,17 @@ package com.zara.Zara.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity(name = "commission_settings")
 @Data
 public class CommissionSetting {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private BigDecimal ceil;
     private BigDecimal top;
